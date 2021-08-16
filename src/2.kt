d49009d6a2b5659e6e57570bfaf0161b92fac8ab
Lavish Swarnkar <lavishswarnkar@gmail.com>
2021-08-16 10:57:41 +0530

    2Done

--- src/2.kt
+++ src/2.kt
@@ -1,24 +1,15 @@
-import java.util.*
-
 fun main() {
-    //test(intArrayOf(1,0,2,0,3,4))
+    test(intArrayOf(1,0,2,0,3,4))
     test(intArrayOf(1,0,2,0,0,4))
-    //test(intArrayOf(1,0,0))
+    test(intArrayOf(1,0,0))
+    test(intArrayOf(8,4,5,0,0,0,0,7))
+    test(intArrayOf(1,0,2,3,0,4,5,0))
 }
 
 fun test(arr: IntArray) {
     duplicateZeros(arr)
     println(arr.contentToString())
 }
-
-/**
- * [1,0,2,0,3,4]
- * [1,0,0,2,0,0]
- * noOfZeroes = 2
- * [1,0,2,0,3,4]
- * [1,0,0,2,0,0]
- *    .       -
- */
 
 fun duplicateZeros(arr: IntArray): Unit {
     //Count #0
@@ -30,13 +21,25 @@ fun duplicateZeros(arr: IntArray): Unit {
     if(noOfZeroes == 0) return
 
     //Start copying from this index
-    val start = arr.size - noOfZeroes - 1
+    val end = arr.size - 1
 
-    //Copy to this index in arr
-    var ptr = arr.size - 1
+    //Copy to this index in arr (may be out of bounds)
+    var ptr = arr.size - 1 + noOfZeroes
 
     //Reverse iteration for in-place shifting
-    for(i in start downTo  0){
+    for(i in end downTo  0){
+
+        //Out of bounds check
+        if(ptr > arr.size - 1){
+            ptr--
+
+            if(arr[i] == 0)
+                if(ptr == arr.size - 1) arr[ptr--] = 0 else ptr--
+
+            continue
+        }
+
+        //Copy
         arr[ptr--] = arr[i]
 
         //Copy twice if 0
